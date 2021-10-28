ba08bb527993f77e4f3ffed78df6decfd46cff06
maks.user99 <maks.user99@gmail.com>
2021-10-28 12:40:09 +0300

    should parse graph

--- src/stores/GraphStore.ts
+++ src/stores/GraphStore.ts
@@ -12,10 +12,25 @@ export class GraphStore {
   }
 
   loadGraph = async () => {
-    this.isLoading = true;
-    const response = await this.rootStore.api.get('/node/name/GES');
-    this.graphJSON = typeof response.data === 'string' ? response.data : '';
-    console.log('response: ', response, JSON.parse(response.data as string));
-    this.isLoading = false;
+    try {
+      this.isLoading = true;
+      const response = await this.rootStore.api.get('/node/name/GES');
+      this.graphJSON = typeof response.data === 'string' ? response.data : '';
+      const parsed = JSON.parse(this.graphJSON);
+      this.graph = this.parseGraph(parsed);
+      console.log('response: ', response, JSON.parse(response.data as string));
+      this.isLoading = false;
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  parseGraph(rawGraph: any): Graph {
+    const { id, name, interest, height, childs } = rawGraph;
+    const graph = new Graph({ id, name, interest, height });
+    graph.childs = Array.isArray(childs)
+    ? childs.map(child => this.parseGraph(child))
+    : [];
+    return graph;
   }
 }
