bddb2787a067fd6a2113edb13624f1fbf62003ea
maks.user99 <maks.user99@gmail.com>
2021-11-29 19:33:51 +0200

    migrate from webpack to vite

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,10 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-if (module.hot) {
-  module.hot.accept();
-}
---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import envCompatible from 'vite-plugin-env-compatible';
+import { injectHtml } from 'vite-plugin-html';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import viteTS from 'vite-plugin-ts';
+import reactJsx from 'vite-react-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: /^~/,
+        replacement: ''
+      },
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ],
+    extensions: [
+      '.mjs',
+      '.js',
+      '.ts',
+      '.jsx',
+      '.tsx',
+      '.json',
+      '.vue'
+    ]
+  },
+  plugins: [
+    viteCommonjs(),
+    envCompatible(),
+    injectHtml(),
+    viteTS(),
+    reactJsx()
+  ],
+  build: {}
+})
