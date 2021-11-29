a22145ceac123398c894348f292a3b91821234ba
maks.user99 <maks.user99@gmail.com>
2021-11-29 16:36:45 +0200

    create signup form

--- src/scenes/SignUp/index.tsx
+++ src/scenes/SignUp/index.tsx
@@ -1,14 +1,34 @@
 import { Button, Grid, Link, Paper, TextField, Typography } from '@mui/material';
 import { RouteComponentProps, Link as RouterLink } from "@reach/router";
 import { observer } from "mobx-react";
+import { SubmitHandler, useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { Form } from '../../components/Form';
 import { Scene } from '../../components/Scene';
 
-interface IProps extends RouteComponentProps {}
+interface IFormData {
+  login: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+interface IProps extends RouteComponentProps { }
+
+const schema = yup.object({
+  login: yup.string().required(),
+  password: yup.string().required(),
+  passwordConfirmation: yup.string().required()
+}).required();
+
 
 const SignUp = (props: IProps) => {
-  const submitHandler = (e: any) => {
-    e.preventDefault();
+  const { control, formState: { errors }, handleSubmit } = useForm<IFormData>({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit: SubmitHandler<IFormData> = data => {
+    console.log(data);
   }
 
   return (
@@ -17,13 +37,58 @@ const SignUp = (props: IProps) => {
         component='main'
         elevation={10}
         sx={{ margin: '0 auto' }}>
-        <Form onSubmit={submitHandler}>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Typography className='title' sx={{ mb: 1.5 }}>
             Sign up
           </Typography>
-          <TextField label='login' id='login' sx={{ mb: 1.5 }} />
-          <TextField label='password' id='password' type='password' sx={{ mb: 1.5 }} />
-          <TextField label='confirm password' id='password-confirm' type='password' sx={{ mb: 1.5 }} />
+
+
+          <Controller
+            control={control}
+            name='login'
+            defaultValue=''
+            rules={{ required: true }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label='login'
+                error={!!errors.login}
+                sx={{ mb: 1.5 }} />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name='password'
+            defaultValue=''
+            rules={{ required: true }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label='password'
+                type='password'
+                error={!!errors.password}
+                sx={{ mb: 1.5 }}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name='passwordConfirmation'
+            defaultValue=''
+            rules={{ required: true }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label='confirm password'
+                type='password'
+                error={!!errors.passwordConfirmation}
+                sx={{ mb: 1.5 }}
+              />
+            )}
+          />
+
           <Button type='submit' variant='contained' sx={{ mb: 1.5 }} fullWidth>Create account</Button>
           <Link component={RouterLink} to='/login'>
             Login in
